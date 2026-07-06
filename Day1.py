1ec037ba65a08c7e02e2f1ad401f0d04e400cf6d
CrazyWizrd <ojasch457@gmail.com>
2026-07-06 14:34:18 +0530

    Day1

---
+++ Day1.py
@@ -0,0 +1,13 @@
+print("Hello And Welcome to this Amazing Band Name Generator\nI Will ask you some Questions")
+print("What is the Name of the City in which you grew?")
+city = input()
+print("What is the name of your Pet?")
+color = input()
+print("Found a name: " + city + " " + color)
+
+# # Alternative
+#
+# print("Hello And Welcome to this Amazing Band Name Generator\nI Will ask you some Questions")
+# city = input("What is the Name of the City in which you grew?\n")
+# color = input("What is the name of your Pet?\n")
+# print("Found a name: " + city + " " + color)
